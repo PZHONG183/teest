c803f671a21fd3ee480dafc407dba2a0a59b5b3b
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-17 00:36:22 +0800

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,15 @@
+// functions.js
+function func1(input) {
+    console.log("Function 1 executed with input:", input);
+    return input + " + func1";
+}
+
+function func2(input) {
+    console.log("Function 2 executed with input:", input);
+    return input + " + func2";
+}
+
+function func3(input) {
+    console.log("Function 3 executed with input:", input);
+    return input + " + func3";
+}
