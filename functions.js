e6c87d5098a2578b8f1ba24cbc70e60a89452810
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-17 00:57:45 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,15 +1,20 @@
 // functions.js
-function func1(input) {
-    console.log("Function 1 (加法) executed with input:", input);
-    return input + 2; // 加法：输入值加2
+function funcA(input) {
+    console.log("Function A executed with input:", input);
+    return input + 2;
 }
 
-function func2(input) {
-    console.log("Function 2 (减法) executed with input:", input);
-    return input - 1; // 减法：输入值减1
+function funcB(input) {
+    console.log("Function B executed with input:", input);
+    return input * 3;
 }
 
-function func3(input) {
-    console.log("Function 3 (乘法) executed with input:", input);
-    return input * 3; // 乘法：输入值乘以3
+function funcC(input) {
+    console.log("Function C executed with input:", input);
+    return input - 4;
+}
+
+function funcD(input) {
+    console.log("Function D executed with input:", input);
+    return input / 5;
 }
