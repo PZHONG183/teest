aca79986b7e72970bdffde3a1ece9e01f368f776
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-17 17:03:48 +0800

    Update scriptA.js

--- scriptA.js
+++ scriptA.js
@@ -6,3 +6,10 @@ function funcA(input) {
 function execute(input) {
     return funcA(input);
 }
+
+// scriptA.js
+window.executeA = function(input) {
+    console.log("Executing Script A with input:", input);
+    return input + 2;
+};
+
