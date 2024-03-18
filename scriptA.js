6219e32dbad14cd6bb866ac55573bc2291ff1837
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-18 20:11:25 +0800

    Update scriptA.js

--- scriptA.js
+++ scriptA.js
@@ -13,3 +13,13 @@ window.executeA = function(input) {
     return input + 2;
 };
 
+document.addEventListener('DOMContentLoaded', function() {
+    // 确保DOM完全加载后再尝试获取元素
+    window.flowVolumeElement = document.getElementsByName('FV')[0]; // 获取FLOW VOLUME输入框
+     window.flowVolume = flowVolumeElement.value; // 获取输入框的值 
+
+   console.log("from script _FLOW VOLUME, Nm3/hr:", flowVolume); // 打印获取的值
+
+    // 这里可以根据需要对flowVolume进行进一步处理
+    // 例如: 更新其他元素的值或执行某些计
+});
