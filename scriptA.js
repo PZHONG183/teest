f81f68a77dcb607b16c95c85bc8f46a493c4056b
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-18 22:04:48 +0800

    Update scriptA.js

--- scriptA.js
+++ scriptA.js
@@ -1,20 +1,8 @@
 // scriptA.js
-function executeA(inputValues) {
-    console.log("Executing Script A with input:", inputValues);
-    // 示例操作：将每个输入值增加10%
-    let modifiedValues = inputValues.map(value => value * 1.1);
-    console.log("Modified values by Script A:", modifiedValues);
-    return modifiedValues;
-}
-
-// 添加获取和打印表格值的功能
-document.addEventListener('DOMContentLoaded', function() {
-    let LG = Number(document.getElementsByName('LG')[0].value);
-    let FV = Number(document.getElementsByName('FV')[0].value);
-    let N2 = Number(document.getElementsByName('N2')[0].value);
-    let T = Number(document.getElementsByName('T')[0].value);
-
-    // 将获取的值作为数组传递给 executeA 函数
-    let inputValues = [LG, FV, N2, T];
-    executeA(inputValues);
-});
+// scriptA.js
+window.executeA = function(LG, FV, N2, T) {
+    console.log("Executing Script A with input:", LG, FV, N2, T);
+    // 示例操作：将 FV 增加 10%
+    FV = FV * 1.1;
+    return [LG, FV, N2, T];
+};
