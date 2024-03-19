b1de3a6594bde235706a69f7813b2ae88a797156
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-19 10:27:24 +0800

    Update scriptA.js

--- scriptA.js
+++ scriptA.js
@@ -3,6 +3,6 @@
 window.executescriptA = function(LG, FV, N2, T) {
     console.log("Executing Script A with input:", LG, FV, N2, T);
     // 示例操作：将 FV 增加 10%
-    FV = FV * 1.1;
+    LG = LG * 1.1;
     return [LG, FV, N2, T];
 };
