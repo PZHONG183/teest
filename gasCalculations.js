88d2afa947fc8f12c0003e37325462dc14a5d0af
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-19 21:18:48 +0800

    Update gasCalculations.js

--- gasCalculations.js
+++ gasCalculations.js
@@ -1,3 +1,34 @@
+  var T = parseFloat(document.getElementsByName('T')[0].value) || 0;
+  var T_out = parseFloat(document.getElementsByName('T_out')[0].value) || 0;
+  
+  var N2 = parseFloat(document.getElementsByName('N2')[0].value) || 0;
+  var O2 = parseFloat(document.getElementsByName('O2')[0].value) || 0;
+  var CO2 = parseFloat(document.getElementsByName('CO2')[0].value) || 0;
+  var H2O = parseFloat(document.getElementsByName('H2O')[0].value) || 0;
+  var SO2 = parseFloat(document.getElementsByName('SO2')[0].value) || 0;
+  var NO2 = parseFloat(document.getElementsByName('NO2')[0].value) || 0;
+  var HCL = parseFloat(document.getElementsByName('HCL')[0].value) || 0;
+  var H2S = parseFloat(document.getElementsByName('H2S')[0].value) || 0;
+  var demister = parseFloat(document.getElementsByName('demister')[0].value) || 0;
+  var demister_time = parseFloat(document.getElementsByName('demister_time')[0].value) || 0;
+  var demister_number = parseFloat(document.getElementsByName('demister_number')[0].value) || 0;
+  var drug_T = parseFloat(document.getElementsByName('drug_T')[0].value) || 0;
+  var demister_T = parseFloat(document.getElementsByName('demister_T')[0].value) || 0;
+
+ 
+  var DE_SOX = parseFloat(document.getElementsByName('DE_SOX')[0].value) || 0;
+  var DE_NOX = parseFloat(document.getElementsByName('DE_NOX')[0].value) || 0;
+  var DE_HCL = parseFloat(document.getElementsByName('DE_HCL')[0].value) || 0;
+  var DE_H2S = parseFloat(document.getElementsByName('DE_H2S')[0].value) || 0;  
+    
+  var P = parseFloat(document.getElementsByName('P')[0].value) || 0;
+  var FV = parseFloat(document.getElementsByName('FV')[0].value) || 0;
+
+  var closestT = findClosestRowValue(T);
+  var closestT_out = findClosestRowValue(T_out);
+
+
+
 export const data = [
   ['25', '20'],
   ['45', '40'],
