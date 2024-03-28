abf99b64d8436328eac3a5632b2f77f204b87cb6
PZHONG183 <163366522+PZHONG183@users.noreply.github.com>
2024-03-28 10:34:09 +0800

    Update scriptB.js

--- scriptB.js
+++ scriptB.js
@@ -1,8 +1,8 @@
-window.executescriptB = function(LG, FV, N2, T) {
-    console.log("Executing Script B with input:", LG, FV, N2, T);
+window.executescriptB = function(LG, FV, T) {
+    console.log("Executing Script B with input:", LG, FV, T);
     // 示例操作：将 N2 减少 5%
     N2 = N2 * 0.95;
     LG = LG -1;
-    console.log("Executing Script B with output:", LG, FV, N2, T);
+    console.log("Executing Script B with output:", LG, FV, T);
     return [LG, FV, N2, T];
 };
